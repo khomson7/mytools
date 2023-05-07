213ac9b889a3df02edd14ff31bf4565a440b2d6f
คมน์ ชุ่มสูงเนิน <khomson7@gmail.com>
2023-05-07 21:58:03 +0700

    update new!

--- README.md
+++
@@ -1,2 +0,0 @@
-# mytools
-pcu tools
--- include/database.php
+++
@@ -1,7 +0,0 @@
-<?php 
-include('MySQLConnection.php');
-include('ApiClass.php');
-define("_URL_", "http://127.0.0.1:3011" ) ;
-$connection = new MySQLConnection("127.0.0.1", "sa", "sa", "hosxp_pcu");
-
-?>
